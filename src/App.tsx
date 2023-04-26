f2b6853bb01377e13bbd0a8847c518165cce3a44
kaischaefer2000 <kai305@web.de>
2023-04-26 20:10:10 +0200

    add maps table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as d3 from "d3";
+import BasicTable from "./Table";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
+  const [locations, setLocations] = React.useState<string[] | null>(null);
+  const [cleanedLocations, setCleanedLocations] = React.useState<
+    { name: string; count: number }[] | null
+  >(null);
+
+  React.useEffect(() => {
+    const locationCountMap = new Map<string, number>();
+
+    if (locations) {
+      // add count of location and sum up equal locations
+      locations.forEach((location) => {
+        locationCountMap.set(
+          location,
+          (locationCountMap.get(location) || 0) + 1
+        );
+      });
+
+      // create the array of objects filter and sort them
+      const loc = Array.from(locationCountMap.entries())
+        .map(([name, count]) => ({
+          name,
+          count,
+        }))
+        .filter((location) => location.count > 5)
+        .sort((a, b) => b.count - a.count);
+
+      // seperate name into first and lastValue
+      const transformedLocations = loc.map((location) => {
+        const commaIndex = location.name.includes(",")
+          ? location.name.indexOf(",")
+          : undefined;
+        const firstValue =
+          commaIndex !== undefined
+            ? location.name.slice(0, commaIndex).trim()
+            : location.name;
+        const lastValue =
+          commaIndex !== undefined
+            ? location.name.slice(commaIndex + 1).trim()
+            : "";
+        const lastValueComparable =
+          commaIndex !== undefined
+            ? location.name
+                .slice(commaIndex + 1)
+                .trim()
+                .toLocaleLowerCase()
+                .replace(/[^a-zäöüß\s]/g, "")
+            : "";
+
+        return { ...location, firstValue, lastValue, lastValueComparable };
+      });
+
+      const wordsArray: string[] = [];
+      transformedLocations.forEach((obj) => {
+        const words = obj.lastValueComparable.split(/\s+/);
+        words.forEach((word) => {
+          if (word !== "") {
+            wordsArray.push(word);
+          }
+        });
+      });
+      const cityMap = new Map<string, number>();
+
+      if (wordsArray) {
+        // add count of location and sum up equal locations
+        wordsArray.forEach((location) => {
+          cityMap.set(location, (cityMap.get(location) || 0) + 1);
+        });
+
+        // create the array of objects filter and sort them
+        const cities = Array.from(cityMap.entries())
+          .map(([name, count]) => ({
+            name,
+            count,
+          }))
+          .filter((location) => location.count > 3)
+          .sort((a, b) => b.count - a.count);
+        console.log(cities);
+      }
+
+      setCleanedLocations(transformedLocations);
+    }
+  }, [locations]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const handleFileRead = (e: ProgressEvent<FileReader>) => {
+    const content = e.target?.result as string;
+    const divs = new DOMParser()
+      .parseFromString(content, "text/html")
+      .querySelectorAll(".mdl-typography--body-1");
+
+    if (divs !== undefined) {
+      let allSearches: string[] = [];
+
+      divs.forEach(function (div) {
+        // loop through each div tag
+        const anchorTags = div.getElementsByTagName("a"); // get all anchor tags inside the div tag
+
+        for (let i = 0; i < anchorTags.length; i++) {
+          // loop through each anchor tag
+          const anchorText = anchorTags[i].innerText; // get the inner text of the anchor tag
+          allSearches.push(anchorText);
+        }
+      });
+
+      setLocations(allSearches);
+    }
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = handleFileRead;
+      reader.readAsText(selectedFile);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div style={{ padding: "50px", background: "lightgrey" }}>
+        <h2>Upload the HTML file: 'MeineAktivitäten Maps'</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <b>Path:</b> takeout-20230331T124008Z-001\Takeout\Meine
+          Aktivitäten\Maps\MeineAktivitäten Maps.html
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <input type='file' accept='.html' onChange={handleFileUpload} />
+          <Button
+            onClick={handleUpload}
+            variant='contained'
+            style={{ marginLeft: "50px" }}
+          >
+            Upload
+          </Button>
+        </div>
+      </div>
+
+      {cleanedLocations ? <BasicTable locations={cleanedLocations} /> : null}
     </div>
   );
 }
---
+++ src/Table.tsx
@@ -0,0 +1,49 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+export default function BasicTable({ locations }: any) {
+  return (
+    <div
+      style={{ paddingLeft: "50px", paddingRight: "50px", marginTop: "20px" }}
+    >
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <b>Location</b>
+              </TableCell>
+              <TableCell align='left'>
+                <b>City?</b>
+              </TableCell>
+              <TableCell align='right'>
+                <b>Count</b>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {locations.map((location: any) => (
+              <TableRow
+                key={location.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  {location.name}
+                </TableCell>
+                <TableCell align='left'>
+                  {location.lastValueComparable}
+                </TableCell>
+                <TableCell align='right'>{location.count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
